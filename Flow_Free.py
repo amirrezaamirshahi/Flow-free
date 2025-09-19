946ca1cfdc6d6e0254b32f1cc64b1e9410232123
Amirreza Amirshahi <amirreza2002amirshahi@gmail.com>
2025-09-19 06:24:19 +0330

    first commit

---
+++ Flow_Free.py
@@ -0,0 +1,272 @@
+import itertools
+import time
+from collections import defaultdict
+from queue import PriorityQueue as PQ
+from copy import deepcopy
+from termcolor import colored
+import numpy as np
+import pygame
+import sys
+
+np.set_printoptions(linewidth=120)
+
+
+class Dot:
+    def __init__(self, color: str, cord: tuple[int, int], dot: bool):
+        self.color: str = color
+        self.loc: tuple[int, int] = cord
+        self.is_dot: bool = dot
+
+    DOT = "\u25a0"
+    LINE = "\u25AA"
+
+    @property
+    def icon(self) -> str:
+        return self.DOT if self.is_dot else self.LINE
+
+    def __sub__(self, other):  # manhattan
+        return sum(abs(z1 - z2) for z1, z2 in zip(self.loc, other.loc))
+
+    def copy(self):
+        return self.__class__(self.color, self.loc, self.is_dot)
+
+    def __repr__(self) -> str:
+        return colored(self.icon, self.color)
+
+
+class DotGraph:
+    EMPTY = "white"
+
+    def __init__(self, size: int, pairs: dict[str, list[tuple[int, int]]]):
+        self.recents = defaultdict(int)
+        self.size = size
+        self.mat: np.ndarray = np.array(
+            [
+                Dot(self.EMPTY, cord, False)
+                for cord in itertools.product(range(size), repeat=2)
+            ],
+            dtype=Dot,
+        ).reshape((size, size))
+        self.pairs: dict[str, list[Dot]] = defaultdict(tuple)
+        for col, cords in pairs.items():
+            for cord in cords:
+                dot: Dot = self.mat[cord]
+                dot.color, dot.is_dot = col, True
+                self.pairs[col] += (dot,)
+
+    def copy(self):
+        copy = self.__class__(0, {})
+        copy.recents = deepcopy(self.recents)
+        size = copy.size = self.size
+        copy.mat = np.array(
+            [dot.copy() for dot in self.mat.flat],
+            dtype=Dot,
+        ).reshape((size, size))
+        copy.pairs = {}
+        for color, (d1, d2) in self.pairs.items():
+            copy.pairs[color] = (copy.mat[d1.loc], copy.mat[d2.loc])
+        return copy
+
+    @property
+    def solved(self):
+        return all(x.color != self.EMPTY for x in self.mat.flat)
+
+    def order_colors(self):
+        return sorted(
+            self.pairs.items(),
+            key=lambda item: (self.recents[item[0]], Dot.__sub__(*item[1])),
+        )
+
+    def get_all_near_dots(self, dot: Dot) -> list[Dot]:
+        nears = []
+        r, c = dot.loc
+        for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            rrcc = r + dr, c + dc
+            if -1 in rrcc:
+                continue
+            try:
+                next_dot: Dot = self.mat[rrcc]
+            except IndexError:
+                continue
+            nears.append(next_dot)
+        return nears
+
+    def get_near_dots(self, orig: Dot, dest: Dot) -> list[Dot]:
+        pq = PQ(4)
+        for next_dot in self.get_all_near_dots(orig):
+            if next_dot.color == self.EMPTY:
+                pq.put((next_dot - dest, next_dot.loc, next_dot))
+        return [x[-1] for x in pq.queue]
+
+    def check_path(self, start: Dot, end: Dot, color: str):
+        rows = cols = self.size
+        stack = [start]
+        visited = np.full((rows, cols), False, dtype=bool)
+        while stack:
+            dot = stack.pop()
+            if dot == end:
+                return True
+            visited[dot.loc] = True
+            for near_dot in self.get_all_near_dots(dot):
+                if near_dot.color in {self.EMPTY, color} and not visited[near_dot.loc]:
+                    stack.append(near_dot)
+        return False
+
+    @property
+    def good(self):
+        for color, (d1, d2) in self.pairs.items():
+            if not self.check_path(d1, d2, color):
+                return False
+        for dot in self.mat.flat:
+            dot: Dot
+            color = dot.color
+            nears = [x.color for x in self.get_all_near_dots(dot)]
+            if dot.color == self.EMPTY:
+                if self.EMPTY in nears:
+                    continue
+                if len(set(nears)) == 1:
+                    if Dot.__sub__(*self.pairs[nears[0]]) == 1:
+                        return False
+            elif all(c != color for c in nears):
+                if self.EMPTY not in nears:
+                    return False
+            elif nears.count(color) >= 3:
+                return False
+        return True
+
+    def do_dots(self) -> tuple[bool, list[Dot]]:
+        for color, dots in self.order_colors():
+            if Dot.__sub__(*dots) <= 1:
+                continue
+            for i, dot in enumerate(dots):
+                match len(nears := self.get_near_dots(dot, dot)):
+                    case 0:
+                        return False, []  # bad
+                    case 1:
+                        near = nears[0]
+                        self.pairs[color] = (near, dots[1 - i])[:: -2 * i + 1]
+                        near.color = color
+                        return True, nears
+        return True, []
+
+    def do_if_not_good(self, po, ch):
+        self.pairs = po
+        for cc in ch:
+            cc.color = self.EMPTY
+
+    def solver(self, first=True):
+        while True:
+            if not self.good:
+                return
+            if self.solved:
+                return self
+            if not first:
+                break
+            match self.do_dots():
+                case (False, _):
+                    return
+                case (True, []):
+                    break
+        if first:  # show after doing dots
+            print(self.mat)
+        for color, (orig, dest) in self.order_colors():
+            if (orig - dest) > 1:  # if not already connected
+                for near_dot in self.get_near_dots(orig, dest):
+                    # ************************************
+                    near_dot.color = color
+                    self.pairs[color] = (near_dot, dest)
+                    self.recents[color] -= 1
+                    # ************************************
+                    if self.solver(False):
+                        return self
+                    # ?????????????????????????????????????
+                    self.recents[color] += 1
+                    self.pairs[color] = (orig, dest)
+                    near_dot.color = self.EMPTY
+                    # ?????????????????????????????????????
+
+    def show_solve_show(self):
+        print("{0}x{0},".format(self.size), len(self.pairs), "colors")
+        print(self.mat)
+        t = time.time()
+        self.solver()
+        print(time.time() - t)
+        print(self.mat, "\n")
+
+    def display(self):
+        pygame.init()
+        cell_size = 50
+        width, height = self.size * cell_size, self.size * cell_size
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Dot Graph Solver")
+
+        colors = {
+            "red": (255, 0, 0),
+            "green": (0, 255, 0),
+            "blue": (0, 0, 255),
+            "yellow": (255, 255, 0),
+            "magenta": (255, 0, 255),
+            "cyan": (0, 255, 255),
+            "grey": (192, 192, 192),
+            "white": (255, 255, 255),
+            "black": (0, 0, 0),
+            "light_yellow": (255, 255, 153),
+            "light_red": (255, 102, 102),
+            "light_green": (153, 255, 153),
+        }
+
+        def draw_grid():
+            for x in range(0, width, cell_size):
+                pygame.draw.line(screen, colors["black"], (x, 0), (x, height))
+            for y in range(0, height, cell_size):
+                pygame.draw.line(screen, colors["black"], (0, y), (width, y))
+
+        def draw_dots():
+            for dot in self.mat.flat:
+                color = colors[dot.color]
+                r, c = dot.loc
+                rect = pygame.Rect(c * cell_size, r *
+                                   cell_size, cell_size, cell_size)
+                pygame.draw.rect(screen, color, rect)
+                if dot.is_dot:
+                    pygame.draw.circle(
+                        screen, colors["black"], rect.center, cell_size // 4
+                    )
+
+        clock = pygame.time.Clock()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            screen.fill(colors["white"])
+            draw_grid()
+            draw_dots()
+            pygame.display.flip()
+            clock.tick(60)
+
+
+#Input table
+input_data = {
+    "green": [(0, 5), (4, 3)],
+    "magenta": [(2, 1), (3, 5)],
+    "red": [(4, 1), (3, 3)],
+    "blue": [(5, 3), (4, 5)],
+    "light_yellow": [(2, 2), (4, 4)],
+}
+
+# input_data = {
+#     "green": [(0, 0), (0, 6)],
+#     "magenta": [(1, 0), (1, 6)],
+#     "red": [(2, 0), (2, 6)],
+#     "blue": [(3, 0), (3, 6)],
+#     "yellow": [(4, 0), (4, 6)],
+#     "cyan": [(5, 0), (5, 6)],
+#     "light_yellow": [(6, 0), (6, 6)],
+# }
+
+# Create and solve the DotGraph
+dot_graph = DotGraph(6, input_data)
+dot_graph.show_solve_show()
+dot_graph.display()
